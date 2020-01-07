79d8cee6bfeccb0c138d715b5b44850ff310dfdd
brown5628 <brown5628@gmail.com>
2020-01-07 07:37:49 -0500

    Begin streamlit test

---
+++ uber_pickups.py
@@ -0,0 +1,35 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+import altair as alt 
+
+st.title("Uber pickups in NYC")
+
+date_column = 'date/time'
+data_url = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(data_url, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[date_column] = pd.to_datetime(data[date_column])
+    return data
+
+# Create a text element and let the reader know the data is loading
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe
+data = load_data(10000)
+# Motify the reader that the data was successfully loaded
+data_load_state.text('Loading data...done!')
+
+st.subheader('Raw data')
+st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(
+    data[date_column].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+st.write(hist_values)
