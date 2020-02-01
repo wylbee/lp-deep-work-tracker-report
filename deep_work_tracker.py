7db9169ea5de6860f0eb41bc204643d049743cea
brown5628 <brown5628@gmail.com>
2020-02-01 11:01:28 -0500

    expiramentation around heatmaps

--- deep_work_tracker.py
+++ deep_work_tracker.py
@@ -12,17 +12,69 @@ raw = pd.read_excel("/home/brown5628/projects/lp-deep-work-tracker-report/deep_w
 st.subheader("Graph")
 
 # %% 
-#date_range = 
+heatmap_wrangled = raw.copy()
+heatmap_wrangled = (
+    heatmap_wrangled
+    .assign(year = pd.DatetimeIndex(heatmap_wrangled['date']).year)
+    .assign(year_week_number = pd.DatetimeIndex(heatmap_wrangled['date']).year + heatmap_wrangled['week_number'])
+)
 
-heatmap_wrangled = raw.copy() 
+year_filter_value = 2020
+heatmap_wrangled_filtered = (
+    heatmap_wrangled.copy()
+    .query(f'year=={year_filter_value}')
+)
+
+weekly_dw_goal = 100
+# %%
+base_master_heatmap = alt.Chart(heatmap_wrangled_filtered).transform_joinaggregate(
+    sum_minutes = 'sum(minutes):Q',
+    groupby=["week_number"]
+).transform_calculate(
+    color = 'datum.sum_minutes < 1200 ? "orange" : "blue"'
+)
+
+master_heatmap = base_master_heatmap.mark_rect().encode(
+    alt.X('week_number:O', title='week'),
+    alt.Y('month(date):O', title='month'),
+    color= alt.Color('color:N', scale=None),
+    tooltip = "sum(minutes):Q"
+).properties(
+    title='Deep Work Minutes by Week'
+)
 
 # %%
 
-heatmap = alt.Chart(heatmap_wrangled).mark_rect().encode(
-    x="week_number:O",
+alt_heat = alt.Chart(heatmap_wrangled_filtered, title="Another Heatmap").mark_rect().encode(
+    x='date(date):O',
+    y='month(date):O',
+    color = alt.Color('sum(minutes):Q',scale=alt.Scale(scheme="inferno")),
+    tooltip=[
+        alt.Tooltip('monthdate(date):T', title='Date'),
+        alt.Tooltip('sum(minutes):Q', title='Minutes')
+    ]
+)
+# %%
+
+heatmap = alt.Chart(heatmap_wrangled_filtered).mark_rect().encode(
+    x="year_week_number:O",
     y="weekday:O",
     color="minutes:Q",
     tooltip="minutes:Q"
 )
 
-st.altair_chart(heatmap, width=0)
+stacked_bar = alt.Chart(heatmap_wrangled_filtered).mark_bar().encode(
+    x="year_week_number:O",
+    y="sum(minutes):Q",
+    color="type",
+    tooltip="sum(minutes):Q"
+)
+
+# %%
+st.altair_chart(master_heatmap, width=0)
+
+st.altair_chart(alt_heat)
+
+st.altair_chart(alt.vconcat(heatmap,stacked_bar), width=0)
+
+# %%
